29c42dc0a6ba5c24f33183f9484538a798ebd497
gautam <htgautam2003-spec1>
2026-03-11 01:08:49 +0530

    Remove node_modules and added gitignore

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors()); // Important
+
+app.get("/api", (req, res) => {
+  res.json({
+    name: "Gautam",
+    project: "E-Book Library",
+    status: "Backend Connected 🚀"
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
